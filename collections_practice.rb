162883ec0b3b256f3c3f26d803f37142c6697701
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-18 16:54:10 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+def sort_array_asc(array)
+  array.sort
+end 
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|x| x.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array 
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+  
+  new_array = [] 
+  
+  array.each {|name| new_array << name[2] = "$"}
+ 
+end 
+
+def find_a(array)
+  
+  array.select {|word| word.start_with?("a")}
+  
+end
+
+def sum_array(array)
+  
+  array.inject {|sum, n| sum + n }
+  
+end
+
+def add_s(array)
+  
+  array.each_with_index.collect do |element, index| 
+   if element == array[1]
+     element
+   else
+     element << "s"
+   end
+ end 
+     
+end
+
+
+
+
 
